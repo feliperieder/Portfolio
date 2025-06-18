b5d38785b3a646d01fb1b3d90fd5ee84a86ca219
feliperieder <feliperieder@gmail.com>
2025-06-18 17:43:26 -0300

    Get in touch button

--- portfolio/src/components/MainComponent.jsx
+++ portfolio/src/components/MainComponent.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import myCSS from "./MainComponent.module.css"
 
+import { HashLink } from 'react-router-hash-link';
+
 const MainComponent = () => {
   return (
     <div>
         <div className= {myCSS.container}>
             <h1 className>Felipe Schmidt Rieder</h1>
             <p className={myCSS.description}>Currently studying Game Development at Unisinos, with experience in both programming and game design.</p>
-            <a href='#' className={myCSS.contact}>Get in touch</a>
+            <HashLink smooth to="#aboutMe_section" className={myCSS.contact}>Get in touch</HashLink>
         </div>
     </div>
   )
